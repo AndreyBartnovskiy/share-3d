863142a0662b3a07e060e9a7fa8136053f84b5f9
Андрей Бартновский <andrejbartnovskij@MacBook-Air-Andrej.local>
2025-03-19 15:24:19 +0300

    clear code

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,13 +1,5 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-//= require rails-ujs
-
 import { Application } from "@hotwired/stimulus"
 import Dropdown from 'stimulus-dropdown'
 
 const application = Application.start()
-
 application.register('dropdown', Dropdown)
-application.debug = false
-window.Stimulus   = application
-
-export { application }
