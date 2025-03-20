7879491db7d9400a01a21355f5380a2ceb765ed3
Андрей Бартновский <andrejbartnovskij@MacBook-Air-Andrej.local>
2025-03-20 03:13:40 +0300

    user invitations/mailer + work with profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,11 +1,13 @@
 class ProfilesController < ApplicationController
   def new
     redirect_to root_path unless current_user.profile.nil?
+
     @profile = Profile.new
   end
 
   def create
     @profile = Profile.new(profile_params)
+
     if @profile.save
       current_user.profile = @profile
       current_user.add_role :admin, @profile
@@ -13,14 +15,13 @@ class ProfilesController < ApplicationController
       current_user.save
       redirect_to root_path, success: "Your profile has been created!"
     else
-    render :new
+      render :new
     end
   end
 
   private
 
   def profile_params
-    {}
-    # params.require(:profile).permit(:name)
+    params.require(:profile).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,26 @@
 Rails.application.routes.draw do
-  get "users/index"
-  get "users/show"
-  get "users/new"
-  get "users/create"
-  get "users/edit"
-  get "users/update"
+  # get "users/index"
+  # get "users/show"
+  # get "users/new"
+  # get "users/create"
+  # get "users/edit"
+  # get "users/update"
+
   devise_for :users, controllers: { registrations: "registrations" }
   resource :profiles
 
+  get "user/me", to: "users#me", as: "my_settings"
+  patch "user/update_me", to: "users#update_me", as: "update_my_settings"
+  get "user/password", to: "users#password", as: "my_password"
+  patch "user/update_password", to: "users#update_password", as: "my_update_password"
+
+  scope "profile", as: "profile" do
+    resources :users
+  end
+
   get "activity/mine"
   get "activity/feed"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
   root to: "activity#mine"
 end
