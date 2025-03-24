4a8b17bc0f2a52fb0cb703ef5781534eb2cb0712
Андрей Бартновский <andrejbartnovskij@MacBook-Air-Andrej.local>
2025-03-24 21:29:56 +0300

    add active storage for models

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,7 @@
 class Model < ApplicationRecord
   belongs_to :user
+  has_one_attached :file # Привязываем 3D-модель к Active Storage
+
+  validates :name, presence: true
+  validates :file, presence: true
 end
