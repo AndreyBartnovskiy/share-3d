2cd7b8adccb73a5c296678a350ad0824df40a493
Андрей Бартновский <andrejbartnovskij@MacBook-Air-Andrej.local>
2025-04-11 09:11:37 +0300

    first version of auto search

--- app/javascript/controllers/model_viewer_controller.js
+++ app/javascript/controllers/model_viewer_controller.js
@@ -1,14 +1,24 @@
 import { Controller } from "@hotwired/stimulus"
 import { ModelEmbed } from "../components/model_embed"
 import { ModelAnalyzer } from "../components/model_analyzer"
+import { ModelArtifactAnalyzer } from "../components/model_artifact_analyzer"
 
 export default class extends Controller {
+  static targets = ['canvas', 'modelUrl', 'loading', 'error', 'stats', 'topology', 'artifacts'];
+
   connect() {
-    this.initializeComponents()
+    this.initializeBabylon();
+    this.initializeComponents();
+  }
+
+  initializeBabylon() {
+    // Инициализация Babylon.js происходит автоматически через babylon-viewer
+    // в HTML-разметке, поэтому здесь ничего не нужно делать
   }
 
   initializeComponents() {
     new ModelEmbed()
-    new ModelAnalyzer()
+    this.modelAnalyzer = new ModelAnalyzer()
+    this.artifactAnalyzer = new ModelArtifactAnalyzer()
   }
 } 
