34b5978e7eb42b3abb9155457bd9e6294372ee36
Андрей Бартновский <andrejbartnovskij@MacBook-Air-Andrej.local>
2025-04-17 14:22:06 +0300

    uv развертка по эффективности

--- lib/uv_analysis/analyze_uv.py
+++ lib/uv_analysis/analyze_uv.py
@@ -38,21 +38,52 @@ def analyze_uv(filepath):
                 elif tcoords is not None and hasattr(tcoords, '__len__') and len(tcoords) > 0:
                     uv_exist = True
                     break
-            # Примитивная эвристика эффективности UV (можно доработать)
+            # Реальный расчет эффективности использования UV-пространства
             uv_efficiency = None
             overlapping_islands = None
             distortion = None
             details = ""
+            uv_efficiency_explanation = ""
             if uv_exist:
-                details = "UV-развёртка обнаружена."
-                uv_efficiency = 1.0  # TODO: Реализовать реальную оценку эффективности
-                overlapping_islands = False  # TODO: Реализовать реальную проверку
-                distortion = False  # TODO: Реализовать реальную проверку
+                total_uv_area = 0.0
+                for mesh in meshes:
+                    tcoords = getattr(mesh, 'texturecoords', None)
+                    if tcoords is not None and len(tcoords) > 0:
+                        uvs = tcoords[0]
+                        # pyassimp может возвращать numpy array или list
+                        if hasattr(uvs, 'tolist'):
+                            uvs = uvs.tolist()
+                        faces = getattr(mesh, 'faces', [])
+                        for face in faces:
+                            if len(face) != 3:
+                                continue  # Только треугольники
+                            try:
+                                uv0 = uvs[face[0]][:2]
+                                uv1 = uvs[face[1]][:2]
+                                uv2 = uvs[face[2]][:2]
+                                # Площадь треугольника по формуле через векторное произведение
+                                area = abs(0.5 * ((uv1[0] - uv0[0]) * (uv2[1] - uv0[1]) - (uv2[0] - uv0[0]) * (uv1[1] - uv0[1])))
+                                total_uv_area += area
+                            except Exception:
+                                continue
+                # Ограничим область UV-карты 1x1
+                total_uv_area = min(total_uv_area, 1.0)
+                uv_efficiency = total_uv_area * 100  # В процентах
+                details = f"UV-развёртка обнаружена. Эффективность использования UV-пространства: {uv_efficiency:.2f}%."
+                if uv_efficiency < 30:
+                    uv_efficiency_explanation = "Низкая эффективность: большая часть UV-карты не используется, текстура может быть размытой."
+                elif uv_efficiency < 70:
+                    uv_efficiency_explanation = "Средняя эффективность: есть неиспользуемые области, можно улучшить развёртку."
+                else:
+                    uv_efficiency_explanation = "Высокая эффективность: UV-пространство используется хорошо, текстура будет качественной."
+                overlapping_islands = None  # Не реализовано
+                distortion = None  # Не реализовано
             else:
                 details = "UV-развёртка отсутствует!"
             return {
                 "uv_exists": uv_exist,
                 "uv_efficiency": uv_efficiency,
+                "uv_efficiency_explanation": uv_efficiency_explanation,
                 "overlapping_islands": overlapping_islands,
                 "distortion": distortion,
                 "details": details
