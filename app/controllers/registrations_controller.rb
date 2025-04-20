129f3d63e2df4c59dd00154d1746d347f39ae3ed
Андрей Бартновский <andrejbartnovskij@MacBook-Air-Andrej.local>
2025-04-20 18:25:43 +0300

    correct auth work

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,27 @@
 class RegistrationsController < Devise::RegistrationsController
   before_action :configure_permitted_parameters
 
+  # После регистрации назначаем роль и создаем профиль для преподавателя
+  def create
+    super do |resource|
+      next unless resource.persisted?
+      if params[:account_type] == 'teacher'
+        # создаем профиль группы и даем локальную роль admin
+        profile = Profile.create(name: "Profile for #{resource.email}")
+        resource.profile = profile
+        resource.add_role :admin, profile
+      else
+        # даем глобальную роль user
+        resource.add_role :user
+      end
+      resource.save
+    end
+  end
+
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) # rubocop:disable Layout/SpaceInsideArrayLiteralBrackets
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
   def after_sign_up_path_for(resource)
